1d71f16a5e68adcaf0b8feeb8dc2f08926c0aeab
zfirstalpha <salim14sadik@gmail.com>
2025-09-15 17:45:30 +0300

    Task 3: Ambient namespaces with CRUD library

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" />
+import { RowID, RowElement } from "./interface";
+
+export declare function insertRow(row: RowElement): RowID;
+export declare function deleteRow(rowId: RowID): void;
+export declare function updateRow(rowId: RowID, row: RowElement): RowID;
