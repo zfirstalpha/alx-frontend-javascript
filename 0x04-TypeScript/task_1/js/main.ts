a1cd9d6243c66766082a120b3c66d0d15e47d236
zfirstalpha <salim14sadik@gmail.com>
2025-09-15 17:01:38 +0300

    Task 1: Teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,25 +1,38 @@
 // Teacher interface
 interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+  readonly firstName: string;       // only set on init
+  readonly lastName: string;        // only set on init
+  fullTimeEmployee: boolean;        // always required
+  yearsOfExperience?: number;       // optional
+  location: string;                 // always required
+  [key: string]: any;               // allow extra properties
 }
 
 // Directors interface extending Teacher
 interface Directors extends Teacher {
-  numberOfReports: number;
+  numberOfReports: number;          // mandatory
 }
+
+// Example teacher
+const teacher1: Teacher = {
+  firstName: "henok",
+  lastName: "lam",
+  fullTimeEmployee: true,
+  location: "Addis Ababa",
+  yearsOfExperience: 5,
+  contract: true,                   // extra property
+};
+
+console.log("Teacher:", teacher1);
 
 // Example director
 const director1: Directors = {
-  firstName: "henok",
-  lastName: "lam",
-  location: "aa",
+  firstName: "roba",
+  lastName: "fike",
   fullTimeEmployee: true,
+  location: "aa",
   numberOfReports: 17,
+  contract: false,                  // extra property allowed
 };
 
-console.log(director1);
+console.log("Director:", director1);
